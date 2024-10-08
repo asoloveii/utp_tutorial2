51f3f47a9bea481235c6fc6b99f6df75c776b988
Z1BIX <maxgolvatenko@gmail.com>
2024-10-08 09:39:57 +0200

    Changes to the add call

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
-        System.out.println(adder.add(1, 2));
+        System.out.println(adder.add(3, 5));
         Subtractor subtractor = new Subtractor();
 
         System.out.println(subtractor.subtract(6, 3));
